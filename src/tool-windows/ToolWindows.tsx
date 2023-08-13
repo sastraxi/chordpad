9e2314e18b0e7fea96f0ede39d72348f09d0fbf8
Cam Gorrie <sastraxi@gmail.com>
2023-08-13 00:25:59 -0400

    block out tool windows a bit

--- src/tool-windows/ToolWindows.tsx
+++ src/tool-windows/ToolWindows.tsx
@@ -1,69 +1,68 @@
-import { Button, Card, CardBody, CardHeader, Divider, HStack, Kbd, Tab, TabList, TabPanel, TabPanels, Table, TableContainer, Tabs, Tbody, Td, Text, Th, Thead, Tr, VStack } from "@chakra-ui/react"
+import { ViewOffIcon } from "@chakra-ui/icons"
+import { Button, Card, CardBody, CardHeader, Divider, HStack, IconButton, Kbd, Spacer, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr, VStack } from "@chakra-ui/react"
 
 const ToolWindows = () => {
   return (
-    <VStack p={6}>
+    <VStack p={6} width="460px">
 
-      <Card background="blue.800" color="white" minW="350px" boxShadow="xl">
-        <CardHeader py={4}>
-          <HStack gap={2}>
-            <Button size="sm">Save</Button>
-            <Button size="sm">Save as...</Button>
-            <Button size="sm"></Button>
-          </HStack>
-        </CardHeader>
-        <Divider />
+      <Card background="red.800" color="white" w="100%" boxShadow="xl">
         <CardBody>
-          <Tabs size="sm" variant="solid-rounded" colorScheme='blue'>
-            <TabList color="white">
-              <Tab color="white">Help</Tab>
-              <Tab color="white">Songs</Tab>
-            </TabList>
-            <TabPanels>
-              <TabPanel>
-                <HStack>
-                  <Kbd color="black">drag</Kbd>
-                  <Text fontSize="sm">to select / move / resize</Text>
-                </HStack>
-                <HStack>
-                  <Kbd color="black">ctrl</Kbd>
-                  <Text fontSize="sm">+</Text>
-                  <Kbd color="black">drag</Kbd>
-                  <Text fontSize="sm">to box select / clone / ripple edit</Text>
-                </HStack>
-              </TabPanel>
-              <TabPanel>
-                <TableContainer>
-                  <Table size='sm'>
-                    <Thead>
-                      <Tr>
-                        <Th color="white">To convert</Th>
-                        <Th color="white">into</Th>
-                        <Th color="white" isNumeric>multiply by</Th>
-                      </Tr>
-                    </Thead>
-                    <Tbody>
-                      <Tr>
-                        <Td>inches</Td>
-                        <Td>millimetres (mm)</Td>
-                        <Td isNumeric>25.4</Td>
-                      </Tr>
-                      <Tr>
-                        <Td>feet</Td>
-                        <Td>centimetres (cm)</Td>
-                        <Td isNumeric>30.48</Td>
-                      </Tr>
-                      <Tr>
-                        <Td>yards</Td>
-                        <Td>metres (m)</Td>
-                        <Td isNumeric>0.91444</Td>
-                      </Tr>
-                    </Tbody>
-                  </Table>
-                </TableContainer>
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
+          <HStack>
+            <Kbd color="black">drag</Kbd>
+            <Text fontSize="sm">to select / move / resize</Text>
+          </HStack>
+          <HStack>
+            <Kbd color="black">ctrl</Kbd>
+            <Text fontSize="sm">+</Text>
+            <Kbd color="black">drag</Kbd>
+            <Text fontSize="sm">to box select / clone / ripple edit</Text>
+          </HStack>
+        </CardBody>
+      </Card>
+
+      <Card background="blue.800" color="white" w="100%" boxShadow="xl">
+        <CardBody py={4}>
+          <VStack gap={4}>
+            <HStack gap={2} w="100%">
+              <Button size="xs">New song</Button>
+              <Button size="xs">Save "My song"</Button>
+              <Spacer />
+              <IconButton
+                size="xs"
+                colorScheme='blue'
+                aria-label='Show / hide'
+                icon={<ViewOffIcon />}
+              />
+            </HStack>
+            <TableContainer w="100%">
+              <Table size="xs" fontSize="14px">
+                <Thead>
+                  <Tr>
+                    <Th color="white">Song name</Th>
+                    <Th color="white" isNumeric>Saved at</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td>My song</Td>
+                    <Td isNumeric>just now</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>abc</Td>
+                    <Td isNumeric>yesterday</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>def</Td>
+                    <Td isNumeric>2 weeks ago</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>ghi</Td>
+                    <Td isNumeric>May 21, 2023</Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </VStack>
         </CardBody>
       </Card>
 
